9109c91b3218d0149a2cf8539eb35c57fccad7c9
sebastianberns <sebastianberns@gmail.com>
2018-07-28 09:35:56 +0200

    Binary accuracy threshold parameter

--- ignite/metrics/binary_accuracy.py
+++ ignite/metrics/binary_accuracy.py
@@ -14,13 +14,21 @@ class BinaryAccuracy(Metric):
     - `y_pred` must be in the following shape (batch_size, ...) and it's elements must be between 0 and 1.
     - `y` must be in the following shape (batch_size, ...)
     """
+    def __init__(self, threshold=0.5, output_transform=lambda x: x):
+        super(BinaryAccuracy, self).__init__(output_transform)
+        self._threshold = threshold
+        self._max = 1
+        self._min = 0
+
     def reset(self):
         self._num_correct = 0
         self._num_examples = 0
 
     def update(self, output):
         y_pred, y = output
-        correct = torch.eq(torch.round(y_pred).type(y.type()), y).view(-1)
+        max, min = torch.tensor(self._max).to(y.device), torch.tensor(self._min).to(y.device)
+        y_pred = torch.where(y_pred > self._threshold, max, min)
+        correct = torch.eq(y_pred.type(y.type()), y).view(-1)
         self._num_correct += torch.sum(correct).item()
         self._num_examples += correct.shape[0]
 
